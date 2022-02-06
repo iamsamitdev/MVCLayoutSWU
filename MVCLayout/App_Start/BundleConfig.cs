bcfb0808a83cf1079c6fc1e03e7415f8372eaef6
Samit Koyom <samitkoyom@gmail.com>
2022-02-06 10:59:12 +0700

    first commit

---
+++ MVCLayout/App_Start/BundleConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace MVCLayout
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/custom").Include("~/Scripts/scripts.js"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/styles.css"));
+        }
+    }
+}
